5b2de082ef8bfb7c584e29ab237958fc8f89d9a7
oneseel <seno1006@gmail.com>
2023-11-11 16:54:59 +0900

    level 2-7

---
+++ src/level2/level2_6/Solution.java
@@ -0,0 +1,11 @@
+package level2.level2_6; // x만큼 간격이 있는 n개의 숫자
+
+class Solution {
+    public long[] solution(int x, int n) {
+        long[] numbers = new long[n];
+        for (int i = 0; i < n; i++) {
+            numbers[i] = x + (long) x * i;
+        }
+        return numbers;
+    }
+}
---
+++ src/level2/level2_7/Solution.java
@@ -0,0 +1,14 @@
+package level2.level2_7; // 자연수 뒤집어 배열로 만들기
+
+class Solution {
+    public int[] solution(long n) {
+        String nStr = Long.toString(n);
+        String[] nStrSplit = nStr.split("");
+        int[] numbers = new int[nStrSplit.length];
+
+        for (int i = 0; i < nStrSplit.length; i++) {
+            numbers[i] = Integer.parseInt(nStrSplit[nStrSplit.length - i -1]);
+        }
+        return numbers;
+    }
+}
